41d7b510391111addd67b2a631f39f413527365a
beargame123 <hoyoung7827@gmail.com>
2023-04-05 09:24:49 +0900

    ⚙️:: setting_Project

---
+++ src/test/java/com/example/greenybackend/GreenyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.greenybackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreenyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
